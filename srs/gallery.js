e7260c08dad4ab11a4ebdd326340cc1f6e3c4a1e
AleksandrNebesnyi <a.nebesnyi@gmail.com>
2021-09-13 06:33:31 +0300

    render gallery

--- srs/gallery.js
+++ srs/gallery.js
@@ -1,55 +1,105 @@
-// import { galleryItems } from "./app.js";
+import { galleryItems } from "./app.js";
 
-// const refs = {
-//   galleryContainer: document.querySelector(".js-gallery"),
-//   modal: document.querySelector(".js-lightbox"),
-//   overlay: document.querySelector(".lightbox__overlay"),
-//   lightboxContentEl: document.querySelector(".lightbox__content"),
-//   modalCloseBtn: document.querySelector('[data-action="close-lightbox"]'),
-//   modalImage: document.querySelector(".lightbox__image"),
-// };
-// // console.log(refs.overlay);
-// // Создать разметку галереи
-// function createGalleryItem(items) {
-//   return items
-//     .map(
-//       ({ preview, original, description }) =>
-//         `
-//     <li class="gallery__item">
-//         <a
-//             class="gallery__link"
-//             href="${original}">
-//             <img
-//             class="gallery__image"
-//             src="${preview}"
-//             data-source="${original}"
-//             alt="${description}"
-//             />
-//         </a>
-//     </li>
-//     `
-//     )
-//     .join("");
-// }
+const refs = {
+  galleryContainer: document.querySelector(".js-gallery"),
+  modal: document.querySelector(".js-lightbox"),
+  overlay: document.querySelector(".lightbox__overlay"),
+  lightboxContentEl: document.querySelector(".lightbox__content"),
+  modalCloseBtn: document.querySelector('[data-action="close-lightbox"]'),
+  modalImage: document.querySelector(".lightbox__image"),
+};
+
+console.log(refs.modal);
+// Создать разметку галереи
+function createGalleryItem(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `
+    <li class="gallery__item">
+        <a
+            class="gallery__link"
+            href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </li>
+    `
+    )
+    .join("");
+}
 
 // // Cоздание галереи на основе разметки
 
-// const renderingMarkup = function (markup) {
-//   refs.galleryContainer.insertAdjacentHTML("beforeend", markup);
-// };
+const renderingMarkup = function (markup) {
+  refs.galleryContainer.insertAdjacentHTML("beforeend", markup);
+};
 
 // // Создание галереи
-// renderingMarkup(createGalleryItem(galleryItems));
+renderingMarkup(createGalleryItem(galleryItems));
 // // Открытие модального окна по клику в img
 
-// refs.galleryContainer.addEventListener("click", onOpenModal);
+const onOpenModal = function (event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  refs.modal.classList.add("is-open");
+  console.log("Modal is open");
+};
 
-// const onOpenModal = function (event) {
-//   if (event.target.nodeName !== "IMG") {
-//     return;
+refs.galleryContainer.addEventListener("click", onOpenModal);
+
+const removeClassFromElement = function (className) {
+  const element = document.querySelector(`.${className}`);
+  if (element) {
+    element.classList.remove(className);
+  }
+};
+const onCloseModal = function () {
+  if (refs.modal.classList.contains("is-open")) {
+    removeClassFromElement("is-open");
+    refs.modalImage.src = "";
+    refs.modalImage.alt = "";
+  }
+};
+
+// Закрытие модалки по Btn
+refs.modalCloseBtn.addEventListener("click", onCloseModal);
+
+// Закрытие подалки по Esc
+window.addEventListener("keydown", onEscKeyPress);
+
+function onEscKeyPress(event) {
+  const ESC_KEY_CODE = "Escape";
+  const isEscKey = event.code === ESC_KEY_CODE;
+
+  if (isEscKey) {
+    onCloseModal();
+  }
+}
+
+// const addClassToElement = function (element, className) {
+//   element.classList.add(className);
+// };
+
+// const onImageClick = function (evt) {
+//   evt.preventDefault();
+//   if (evt.target.nodeName === "IMG") {
+//     //Убераю пометку с предыдущей картинки
+//     removeClassFromElement("selected");
+//     //Делаем пометку на выбранной картинке
+//     addClassToElement(evt.target, "selected");
+//     //Открываем модалку
+//     addClassToElement(refModal, "is-open");
+//     //Подмена значения атрибута src элемента img.lightbox__image
+//     refModalImage.src = evt.target.dataset.source;
+//     refModalImage.alt = evt.target.alt;
+//     //Сохраняю текущий индекс картинки который выбрали
 //   }
-//   refs.modal.classList.add("is-open");
-//   console.log("Modal is open");
 // };
 
 // refs.overlay.addEventListener("click", onBackdropClick);
@@ -122,141 +172,144 @@
 
 // //Слушатель Перелистывание галереи при открытом модальном окне
 // refGalleryContainer.addEventListener('keydown', onFlippingGallery);
-import { galleryItems } from "./app.js";
 
-const refGalleryContainer = document.querySelector(".js-gallery");
-const refModal = document.querySelector(".js-lightbox");
-const refModalImage = document.querySelector(".lightbox__image");
-const refModalCloseBtn = document.querySelector(
-  '[data-action="close-lightbox"]'
-);
-const refModalOverlay = document.querySelector(".lightbox__overlay");
+// -----------------------------------------------------
 
-/**
- * Функция создания разметки галереи
- */
-const createGalleryMarkup = function (galleryItems) {
-  return galleryItems
-    .map(
-      ({ preview, original, description }) =>
-        `
-    <li class="gallery__item">
-        <a
-            class="gallery__link"
-            href="${original}">
-            <img
-            class="gallery__image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}"
-            />
-        </a>
-    </li>
-    `
-    )
-    .join("");
-};
+// import { galleryItems } from "./app.js";
 
-/**
- * Функция рендеринга галереи на основе макета разметки
- */
-const renderingMarkup = function (markup) {
-  refGalleryContainer.insertAdjacentHTML("beforeend", markup);
-};
+// const refGalleryContainer = document.querySelector(".js-gallery");
+// const refModal = document.querySelector(".js-lightbox");
+// const refModalImage = document.querySelector(".lightbox__image");
+// const refModalCloseBtn = document.querySelector(
+//   '[data-action="close-lightbox"]'
+// );
+// const refModalOverlay = document.querySelector(".lightbox__overlay");
 
-/**
- * Функция закрытия модального окна
- */
+// /**
+//  * Функция создания разметки галереи
+//  */
+// const createGalleryMarkup = function (galleryItems) {
+//   return galleryItems
+//     .map(
+//       ({ preview, original, description }) =>
+//         `
+//     <li class="gallery__item">
+//         <a
+//             class="gallery__link"
+//             href="${original}">
+//             <img
+//             class="gallery__image"
+//             src="${preview}"
+//             data-source="${original}"
+//             alt="${description}"
+//             />
+//         </a>
+//     </li>
+//     `
+//     )
+//     .join("");
+// };
 
-const onCloseModal = function () {
-  if (refModal.classList.contains("is-open")) {
-    removeClassFromElement("is-open");
-    refModalImage.src = "";
-    refModalImage.alt = "";
-    //Убераю пометку с предыдущей картинки
-    removeClassFromElement("selected");
-  }
-};
+// /**
+//  * Функция рендеринга галереи на основе макета разметки
+//  */
+// const renderingMarkup = function (markup) {
+//   refGalleryContainer.insertAdjacentHTML("beforeend", markup);
+// };
 
-/**
- * Функция убирает указанный класс с элемента
- */
-const removeClassFromElement = function (className) {
-  const element = document.querySelector(`.${className}`);
-  if (element) {
-    element.classList.remove(className);
-  }
-};
+// /**
+//  * Функция закрытия модального окна
+//  */
 
-/**
- * Функция добавляет указанный класс на передаваемый элемент
- */
-const addClassToElement = function (element, className) {
-  element.classList.add(className);
-};
+// const onCloseModal = function () {
+//   if (refModal.classList.contains("is-open")) {
+//     removeClassFromElement("is-open");
+//     refModalImage.src = "";
+//     refModalImage.alt = "";
+//     //Убераю пометку с предыдущей картинки
+//     removeClassFromElement("selected");
+//   }
+// };
 
-/**
- * Функция callback для обработки клика по картинке в галерее
- */
-const onImageClick = function (evt) {
-  evt.preventDefault();
-  if (evt.target.nodeName === "IMG") {
-    //Убераю пометку с предыдущей картинки
-    removeClassFromElement("selected");
-    //Делаем пометку на выбранной картинке
-    addClassToElement(evt.target, "selected");
-    //Открываем модалку
-    addClassToElement(refModal, "is-open");
-    //Подмена значения атрибута src элемента img.lightbox__image
-    refModalImage.src = evt.target.dataset.source;
-    refModalImage.alt = evt.target.alt;
-    //Сохраняю текущий индекс картинки который выбрали
-  }
-};
+// /**
+//  * Функция убирает указанный класс с элемента
+//  */
+// const removeClassFromElement = function (className) {
+//   const element = document.querySelector(`.${className}`);
+//   if (element) {
+//     element.classList.remove(className);
+//   }
+// };
 
-/**
- * Функция перелистывает изображения,
- * при открытой модалке, клавишами "влево" и "вправо"
- */
-const onFlippingGallery = function (evt) {
-  if (refModal.classList.contains("is-open")) {
-    //Текущий выбранный item
-    const selectedImg = evt.currentTarget.querySelector(".selected");
-    const selectedLi = selectedImg.parentNode.parentNode;
-    if (evt.code === "ArrowLeft") {
-      if (selectedLi.previousElementSibling) {
-        removeClassFromElement("selected");
-        let prevImg = selectedLi.previousElementSibling.querySelector("img");
-        addClassToElement(prevImg, "selected");
-        refModalImage.src = prevImg.dataset.source;
-      }
-    } else if (evt.code === "ArrowRight") {
-      if (selectedLi.nextElementSibling) {
-        removeClassFromElement("selected");
-        let nextImg = selectedLi.nextElementSibling.querySelector("img");
-        addClassToElement(nextImg, "selected");
-        refModalImage.src = nextImg.dataset.source;
-      }
-    }
-  }
-};
+// /**
+//  * Функция добавляет указанный класс на передаваемый элемент
+//  */
+// const addClassToElement = function (element, className) {
+//   element.classList.add(className);
+// };
 
-//Создаем галерею
-renderingMarkup(createGalleryMarkup(galleryItems));
+// /**
+//  * Функция callback для обработки клика по картинке в галерее
+//  */
+// const onImageClick = function (evt) {
+//   evt.preventDefault();
+//   if (evt.target.nodeName === "IMG") {
+//     //Убераю пометку с предыдущей картинки
+//     removeClassFromElement("selected");
+//     //Делаем пометку на выбранной картинке
+//     addClassToElement(evt.target, "selected");
+//     //Открываем модалку
+//     addClassToElement(refModal, "is-open");
+//     //Подмена значения атрибута src элемента img.lightbox__image
+//     refModalImage.src = evt.target.dataset.source;
+//     refModalImage.alt = evt.target.alt;
+//     //Сохраняю текущий индекс картинки который выбрали
+//   }
+// };
 
-//Слушатель клика на контейнере
-refGalleryContainer.addEventListener("click", onImageClick);
+// /**
+//  * Функция перелистывает изображения,
+//  * при открытой модалке, клавишами "влево" и "вправо"
+//  */
+// const onFlippingGallery = function (evt) {
+//   if (refModal.classList.contains("is-open")) {
+//     //Текущий выбранный item
+//     const selectedImg = evt.currentTarget.querySelector(".selected");
+//     const selectedLi = selectedImg.parentNode.parentNode;
+//     if (evt.code === "ArrowLeft") {
+//       if (selectedLi.previousElementSibling) {
+//         removeClassFromElement("selected");
+//         let prevImg = selectedLi.previousElementSibling.querySelector("img");
+//         addClassToElement(prevImg, "selected");
+//         refModalImage.src = prevImg.dataset.source;
+//       }
+//     } else if (evt.code === "ArrowRight") {
+//       if (selectedLi.nextElementSibling) {
+//         removeClassFromElement("selected");
+//         let nextImg = selectedLi.nextElementSibling.querySelector("img");
+//         addClassToElement(nextImg, "selected");
+//         refModalImage.src = nextImg.dataset.source;
+//       }
+//     }
+//   }
+// };
 
-//Слушатель Закрытие модалки по кнопке
-refModalCloseBtn.addEventListener("click", onCloseModal);
+// //Создаем галерею
+// renderingMarkup(createGalleryMarkup(galleryItems));
 
-//Слушатель Закрытие модалки по Overlay
-refModalOverlay.addEventListener("click", onCloseModal);
+// //Слушатель клика на контейнере
+// refGalleryContainer.addEventListener("click", onImageClick);
 
-//Слушатель Закрытие подалки по Esc
-window.addEventListener("keydown", (evt) => {
-  if (evt.code === "Escape") onCloseModal();
-});
+// //Слушатель Закрытие модалки по кнопке
+// refModalCloseBtn.addEventListener("click", onCloseModal);
 
-//Слушатель Перелистывание галереи при открытом модальном окне
-refGalleryContainer.addEventListener("keydown", onFlippingGallery);
+// //Слушатель Закрытие модалки по Overlay
+// refModalOverlay.addEventListener("click", onCloseModal);
+
+// //Слушатель Закрытие подалки по Esc
+// window.addEventListener("keydown", (evt) => {
+//   if (evt.code === "Escape") onCloseModal();
+// });
+
+// //Слушатель Перелистывание галереи при открытом модальном окне
+// refGalleryContainer.addEventListener("keydown", onFlippingGallery);
